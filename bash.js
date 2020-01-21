5ccba1a2a55edc3c2a50057d13dc0fd0cc5430ae
Ann-Marie Kemp <aedalenb@gmail.com>
2020-01-21 14:58:23 -0500

    add ls file and function

--- bash.js
+++ bash.js
@@ -1,7 +1,21 @@
-let pwd = require('./pwd')
+let pwd = require('./pwd');
+let ls = require('./ls');
 
 
 
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', pwd.pwdInput);
+process.stdin.on('data', (data) =>{
+  const cmd = data.toString().trim()
+  if(cmd === 'pwd'){
+    pwd();
+  }
+  else if(cmd === 'ls'){
+    ls();
+  }
+  else {
+    process.stdout.write('you typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
+
+});
---
+++ ls.js
@@ -0,0 +1,14 @@
+
+const fsModule = require('fs');
+
+module.exports = function() {
+  fsModule.readdir('./', 'utf8' , (err, files) =>{
+    if(err){
+      throw err
+    }
+    else {
+      process.stdout.write(files.join('\n'))
+      process.stdout.write("prompt > ");
+    }
+  })
+}
